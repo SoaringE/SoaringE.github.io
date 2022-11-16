a667e9f772ea31909d9c0439067e2d3d1884fc49
SoaringE <1305537985@qq.com>
2022-11-16 23:01:55 +0800

    add class

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,3 +1,22 @@
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+
+    hello() {
+        alert('Hello, ' + this.name + '!');
+    }
+}
+class PrimaryStudent extends Student {
+    constructor(name, grade) {
+        super(name); 
+        this.grade = grade;
+    }
+
+    myGrade() {
+        alert('I am at grade ' + this.grade);
+    }
+}
 function showPic(whichpic) {
   var source = whichpic.getAttribute("href");
   var placeholder = document.getElementById("placeholder");
