e0238a6492803196dbcfcb976fe424c53d1edb3d
SoaringE <1305537985@qq.com>
2022-12-11 22:09:52 +0800

    add load

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,3 +1,5 @@
+// classes
+
 class Student {
     constructor(name) {
         this.name = name;
@@ -7,6 +9,7 @@ class Student {
         alert('Hello, ' + this.name + '!');
     }
 }
+
 class PrimaryStudent extends Student {
     constructor(name, grade) {
         super(name); 
@@ -17,6 +20,22 @@ class PrimaryStudent extends Student {
         alert('I am at grade ' + this.grade);
     }
 }
+
+// recursion
+
+function fib(num) {
+    if (num == 1 || num == 0)
+        return 1;
+    var num1 = fib(num - 1);
+    var num2 = fib(num - 2);
+    var result = num1 + num2;
+    return result;
+}
+
+// lambda
+
+var lambda1 = x, y => x + y; 
+
 function showPic(whichpic) {
   var source = whichpic.getAttribute("href");
   var placeholder = document.getElementById("placeholder");
